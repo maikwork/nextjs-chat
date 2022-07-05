ea616e91971af719fa4023986ca024fc8e7dc471
malonchako <malonchako@mts.ru>
2022-07-05 20:45:36 +0300

    Created a config dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UserModule } from './user/user.module';
 
@@ -7,13 +8,16 @@ import { UserModule } from './user/user.module';
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'rbliss',
-            password: 'password',
-            database: 'chat',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_NAME,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
             autoLoadModels: true,
         }),
