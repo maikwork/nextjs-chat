5b6d8dc6fa2aa460abb60a6b377825cf384e8716
malonchako <malonchako@mts.ru>
2022-07-05 20:27:22 +0300

    Added connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UserModule } from './user/user.module';
 
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
-        
-    ]
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'rbliss',
+            password: 'password',
+            database: 'chat',
+            models: [],
+            autoLoadModels: true,
+        }),
+    UserModule]
 })
 export class AppModule {
   
