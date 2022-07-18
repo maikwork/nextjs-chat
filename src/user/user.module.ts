22205c10da6ee1c7f851b2fad51b39e00f17b296
malonchako <malonchako@mts.ru>
2022-07-18 09:59:58 +0300

    Update module for user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UserController } from './user.controller';
+import { User } from './user.model';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports:[
+    SequelizeModule.forFeature([User])
+  ]
 })
 export class UserModule {}
