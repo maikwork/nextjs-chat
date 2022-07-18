90d9c88bb257b0b8ae5bf3c84a0ee76a5d4bb120
malonchako <malonchako@mts.ru>
2022-07-18 09:59:26 +0300

    Update service for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
+    async createUser(dto: CreateUserDTO) {
+        const user = await this.userRepository.create(dto)
+        return user
+    }
+
+    async getAllUser() {
+        const user = await this.userRepository.findAll()
+        return user
+    }
+}
