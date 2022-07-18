c9bf3680fe9b46de1cef0b253a562ceea7b71891
malonchako <malonchako@mts.ru>
2022-07-18 09:59:00 +0300

    Update controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    create(@Body() userDTO: CreateUserDTO) {
+        console.log(userDTO)
+        return this.userService.createUser(userDTO)
+    }
+
+    @Get()
+    findAll() {
+        return this.userService.getAllUser()
+    }
+
+}
